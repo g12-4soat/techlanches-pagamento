b9228290b561d43f4b64a3b8160f9a425029b3d8
vitor <vi.lupinetti@hotmail.com.br>
2024-04-28 20:44:46 -0300

    add teste sucesso criar pagamento e add dockerfile

---
+++ test/TechLanches.Pagamento.UnitTests/Domain/PagamentoTest.cs
@@ -0,0 +1,26 @@
+﻿using TechLanches.Pagamento.Domain.Enums;
+
+namespace TechLanches.Pagamento.UnitTests.Domain
+{
+    [Trait("Domain", "Pagamento")]
+    public class PagamentoTest
+    {
+        [Fact(DisplayName = "Criar pagamento com sucesso")]
+        public void CriarPagamento_DeveRetornarSucesso()
+        {
+            //Arrange    
+            var pedidoId = 1;
+            var valor = 100;
+
+            //Act 
+            var pagamento = new Pagamento.Domain.Aggregates.Pagamento(pedidoId, valor, FormaPagamento.QrCodeMercadoPago);
+
+            //Assert
+            Assert.NotNull(pagamento);
+            Assert.Equal(pedidoId, pagamento.PedidoId);
+            Assert.Equal(valor, pagamento.Valor);
+            Assert.Equal(FormaPagamento.QrCodeMercadoPago, pagamento.FormaPagamento);
+            Assert.Equal(StatusPagamento.Aguardando, pagamento.StatusPagamento);
+        }
+    }
+}
