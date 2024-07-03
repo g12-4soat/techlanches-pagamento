b19239318613aad29ba8b3ce7fb8948c68ad51d1
vitor <vi.lupinetti@hotmail.com.br>
2024-07-02 21:46:13 -0300

    removendo exception teste

--- src/TechLanches.Pagamento/Adapter/Driven/TechLanches.Pagamento.Adapter.RabbitMq/Messaging/RabbitMqService.cs
+++ src/TechLanches.Pagamento/Adapter/Driven/TechLanches.Pagamento.Adapter.RabbitMq/Messaging/RabbitMqService.cs
@@ -44,7 +44,6 @@ namespace TechLanches.Pagamento.Adapter.RabbitMq.Messaging
 
         public void Publicar(IBaseMessage baseMessage, string queueName)
         {
-            throw new Exception();
             var mensagem = Encoding.UTF8.GetBytes(baseMessage.GetMessage());
 
             var properties = _channel.CreateBasicProperties();
